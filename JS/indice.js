a19ca48609b6dfd6fc9c82f26dad438f356c8255
LoretoAlmendro <loretoalmendra.rc@gmail.com>
2023-03-14 15:34:35 -0300

    portafolio 2.0

---
+++ JS/indice.js
@@ -0,0 +1,6 @@
+/* const element = document.getElementById("cualidades_btn");
+element.addEventListener("click", cualidadesOption);
+
+function cualidadesOption() {
+  document.location.href = "index.html";
+} */
