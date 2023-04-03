7fcfc2aaa71044ed0c4d31554bdfe6b1a1e9e9f0
LoretoAlmendro <loretoalmendra.rc@gmail.com>
2023-04-03 11:50:58 -0400

    commit 01/04

--- JS/indice.js
+++
@@ -1,6 +0,0 @@
-/* const element = document.getElementById("cualidades_btn");
-element.addEventListener("click", cualidadesOption);
-
-function cualidadesOption() {
-  document.location.href = "index.html";
-} */
