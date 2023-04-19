3256ceb9c8f2826f32878709fae5c04d128b7a5a
Loreto_menddro <loretoalmendra.rc@gmail.com>
2023-04-18 21:45:20 -0400

    actualizacion 0.3

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 const express = require("express");
-const { engine } = require("express-handlebars");
+/* const { engine } = require("express-handlebars"); */
 const app = express();
 const hbs = require("hbs");
 const path = require("path");
 // const helpers = require("./helpers");
 const bodyParser = require("body-parser");
+const { handlebars } = require("hbs");
 
+//hbs.registerPartial("nav", "{{header}}");
 const partialsDir = path.join(__dirname, "/views/partials");
-hbs.registerPartial("nav", "{{header}}");
 hbs.registerPartials(partialsDir);
 
+app.set('views', path.join(__dirname, 'views'));
 //Establecer la ubicacion de los partials y registralos
-app.engine(
+
+/* app.engine("handlebars", engine());
+app.set('views engine', 'handlebars');
+
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'base',
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    partialDir: [
+        // path to your partials
+        path.join(__dirname, 'views/partials')
+    ]
+})); */
+/* app.engine(
     "hbs",
     engine({
         defaultLayout: "main",
@@ -19,11 +34,10 @@ app.engine(
         partialsDir: "views/partials",
         // helpers: helpers,
     })
-);
-
-app.set("views", path.join(__dirname, "./views"));
+); */
+// app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "hbs");
-app.use("/public", express.static("public"));
+app.use("/public", express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /* app.get('/CSS/index.css', function (req, res) {
@@ -45,6 +59,13 @@ app.get('/cualidades', (req, res) => {
     res.render('cualidades', { title: 'Menu cualidades' });
 });
 
+app.get('/contraste', (req, res) => {
+    res.render('contraste',)
+})
+
+app.get('/tono', (req, res) => {
+    res.render('tono',)
+})
 app.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000');
 });
