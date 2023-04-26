3b83740aabb6a43c339b4b509275979d5e166d43
LoretoAlmendro <loretoalmendra.rc@gmail.com>
2023-04-25 21:38:16 -0400

    avance 0.8

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+import methodOverride from "method-override"
 import express from "express";
 import { join } from "path";
 import path from "path";
 import bodyParser from "body-parser";
 import hbs from "express-hbs";
 import { Sequelize, Model, DataTypes } from "sequelize"; // importar Sequelize
-import { sequelize } from "./API/data/db_connectios.js";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 //Sequelize y DB
 
-import { regionRoutes } from "./API/routes/region.routes.js";
-import { comunaRoutes } from "./API/routes/comuna.routes.js";
-import { personRoutes } from "./API/routes/persona.routes.js";
+// import { regionRoutes } from "./API/routes/region.routes.js";
+// import { comunaRoutes } from "./API/routes/comuna.routes.js";
+// import { personRoutes } from "./API/routes/persona.routes.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -30,11 +30,11 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
 //Middleware=metodo API
-
+app.use(methodOverride("_method", {methods: ["GET", "POST"]}));
 app.use(express.json()),
-app.use (regionRoutes);
-app.use (comunaRoutes);
-app.use (personRoutes);
+// app.use (regionRoutes);
+// app.use (comunaRoutes);
+// app.use (personRoutes);
 
 // define directorio archivos publicos
 app.use(express.static('public'));
@@ -76,6 +76,33 @@ app.get('/luminosidad', (req, res) => {
 app.get('/formulario', (req, res) => {
     res.render('FORMULARIO',)
 })
+
+app.get('/region', async (req, res) => {
+    //haciendo la consulta api
+    const resultado = await fetch (`http://localhost:4000/api/region`);
+    
+    //transformar dato a json
+    const data = await resultado.json();
+
+    res.render('mantenedor', {personas:data});
+});
+
+app.post('/region', async (req, res) => {
+    //haciendo la consulta api
+    const {region} = req.body
+    const body={region:region}
+    console.log(region)
+    const resultado = await fetch (`http://localhost:4000/api/region`,{
+        method: "post",
+        body: JSON.stringify(body),
+        headers:{"content-type":"application/json"}
+    });
+    console.log("ruta post")
+    //transformar dato a json
+    const data = await resultado.json();
+
+    res.redirect('/region');
+});
 
 app.listen(3000, () => {
     console.log('Servidor iniciado en el puerto 3000');
